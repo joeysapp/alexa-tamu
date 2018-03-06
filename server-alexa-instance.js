ffa689a8e98db4e4b3eca10f504392cdb3fabc13
EC2 Default User <ec2-user@ip-172-31-37-183.ec2.internal>
2018-03-06 22:04:02 +0000

    fixing it up for ec2

--- server-alexa-instance.js
+++ server-alexa-instance.js
@@ -1,50 +1,26 @@
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var key =  fs.readFileSync('server-key.pem', 'utf8');
-var cert = fs.readFileSync('server-crt.pem', 'utf8');
-var ca = fs.readFileSync('ca-crt.pem', 'utf8');
+const Alexa = require('alexa-sdk');
 
-var creds = { key: key, cert: cert, ca: ca };
-var express = require("express");
-var app = express();
+exports.handler = function(event, context, callback){
+	const alexa = Alexa.handler(event, context, callback);
+	alexa.appId = "amzn1.ask.skill.82534c6d-52ef-4742-90f3-1945c616832f";
+	alexa.registerHandlers(handlers);
+	alexa.execute();
+};
 
-app.get('/', function(req, res){
-	res.send('hello');
-});
+const handlers = {
+	'HelloWorldIntent' : function(){
+		this.response.speak('Hello world!');
+		this.emit(':responseReady');
+	}
+};
 
-// var alexa = require('./server-alexa-instance.js');
 
-// var http_server = http.createServer(app);
-var https_server = https.createServer(creds, app);
 
-// http_server.listen(65501);
-https_server.listen(65500, function(req, res){
-	console.log("server running");
-});
 
-var path = require("path");
-var useragent = require('useragent');
 
-// console.log(alexa.);
-// app.use(express.static('src'));
 
-// http_server.on('/', function(req, res){
-// 	res.sendFile(path.join(__dirname + '/index.html'));
-// 	var agent = useragent.parse(req.headers['user-agent']);
-// 	console.log(req.headers);
-// })
 
-// app.get('/', function(req, res){
-// 	res.send('hello');
-	// res.sendFile(path.join(__dirname + '/index.html'));
-	// var agent = useragent.parse(req.headers['user-agent']);
-	// console.log(req.headers);
-// })
 
-// app.post('/', function(req, res){
-// 	console.log(req.body);
-// 	// res.sendFile(path.join(__dirname + '/index.html'));
-// 	var agent = useragent.parse(req.headers['user-agent']);
-// 	console.log(req.headers);
-// })
+
+
+
