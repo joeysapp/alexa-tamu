dfafccbe5895c685dcd8e8542b8013ec372947b7
Joseph Sapp <sapp.joey@tamu.edu>
2018-03-06 13:32:00 -0600

    ssl certs are silly and i am sad now

--- server-alexa-instance.js
+++ server-alexa-instance.js
@@ -1,20 +1,27 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var p_key =  fs.readFileSync('sslcert/server.key', 'utf8');
-var cert = fs.readFileSync('sslcert/server.crt', 'utf8');
+var key =  fs.readFileSync('server-key.pem', 'utf8');
+var cert = fs.readFileSync('server-crt.pem', 'utf8');
+var ca = fs.readFileSync('ca-crt.pem', 'utf8');
 
-var creds = { key: p_key, cert: cert };
+var creds = { key: key, cert: cert, ca: ca };
 var express = require("express");
 var app = express();
 
-var alexa = require('./server-alexa-instance.js');
+app.get('/', function(req, res){
+	res.send('hello');
+});
 
-var http_server = https.createServer(app);
+// var alexa = require('./server-alexa-instance.js');
+
+// var http_server = http.createServer(app);
 var https_server = https.createServer(creds, app);
 
-http_server.listen(65500);
-https_server.listen(65500);
+// http_server.listen(65501);
+https_server.listen(65500, function(req, res){
+	console.log("server running");
+});
 
 var path = require("path");
 var useragent = require('useragent');
@@ -22,10 +29,17 @@ var useragent = require('useragent');
 // console.log(alexa.);
 // app.use(express.static('src'));
 
-// app.get('/', function(req, res){
+// http_server.on('/', function(req, res){
 // 	res.sendFile(path.join(__dirname + '/index.html'));
 // 	var agent = useragent.parse(req.headers['user-agent']);
 // 	console.log(req.headers);
+// })
+
+// app.get('/', function(req, res){
+// 	res.send('hello');
+	// res.sendFile(path.join(__dirname + '/index.html'));
+	// var agent = useragent.parse(req.headers['user-agent']);
+	// console.log(req.headers);
 // })
 
 // app.post('/', function(req, res){
