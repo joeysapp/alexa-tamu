a19082ea44669507eaa3e8585c383ca2a82c4d46
Joseph Sapp <sapp.joey@tamu.edu>
2018-03-24 14:24:13 -0500

    fixing application_id

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ const handlers = {
 
 exports.handler = function (event, context, callback) {
 	const alexa = Alexa.handler(event, context, callback);
-	alexa.APP_ID = APP_ID;
+	alexa.appId = APP_ID;
 	// To enable string internationalization (i18n) features, set a resources object.
 	alexa.resources = languageStrings;
 	alexa.registerHandlers(handlers);
